d83084c9bec635b5cff0d41f0c067743e504da7b
Gitari65 <agitari65@gmail.com>
2023-11-03 12:22:59 +0300

    firebase authentication

--- App.js
+++ App.js
@@ -2,11 +2,28 @@ import { StatusBar } from 'expo-status-bar';
 
 import { StyleSheet, Text, View ,Button} from 'react-native';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer} from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Landing from './assets/components/Landing';
 import Register from './assets/components/Register';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/firestore';
+const firebaseConfig = {
+    apiKey: "AIzaSyDzDo2ziayUdfIdx6p0VXfY9xRGyVrtsNM",
+    authDomain: "formm-acb1c.firebaseapp.com",
+    databaseURL: "https://formm-acb1c-default-rtdb.firebaseio.com",
+    projectId: "formm-acb1c",
+    storageBucket: "formm-acb1c.appspot.com",
+    messagingSenderId: "856859093776",
+    appId: "1:856859093776:web:637fcf1df16b5c18175395",
+    measurementId: "G-K35H5RZG1D"
+  };
 
+  if(firebase.apps.length===0){
+    firebase.initializeApp(firebaseConfig);
+  }
 const Stack = createStackNavigator();
 export default function App() {
   return (
