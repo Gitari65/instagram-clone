4adff7b3f08ae0905ef3692fb94bf57739ece246
Gitari65 <agitari65@gmail.com>
2023-11-03 12:23:36 +0300

    firebase authentication

--- assets/components/Landing.js
+++ assets/components/Landing.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { View, Text,Button } from 'react-native'
+import { useNavigation } from '@react-navigation/native';
+
 
 export default function Landing() {
+  const navigation = useNavigation();
   return (
     <View style={{flex:1,justifyContent:'center'}}>
       <Button title="Register" onPress={()=>navigation.navigate('Register')} />
