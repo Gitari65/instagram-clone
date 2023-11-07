c4b7fba517b5e2fbcdf315f5b5561f4cdbd34279
Gitari65 <agitari65@gmail.com>
2023-11-07 18:06:46 +0300

    login form

---
+++ assets/components/Login.jsx
@@ -0,0 +1,17 @@
+import React,{Component} from "react";
+
+import { Button,TextInput } from "react-native";
+
+import React from 'react'
+
+import React, { Component } from 'react'
+
+export class Login extends Component {
+  render() {
+    return (
+      <div>Login</div>
+    )
+  }
+}
+
+export default Login
