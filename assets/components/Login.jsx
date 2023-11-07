55ef46012e4e03640798e39ac013a0ec8816543c
Gitari65 <agitari65@gmail.com>
2023-11-07 18:16:57 +0300

    login form

--- assets/components/Login.jsx
+++ assets/components/Login.jsx
@@ -7,9 +7,16 @@ import React from 'react'
 import React, { Component } from 'react'
 
 export class Login extends Component {
+    constructor(props){
+        super(props);
+        this.state{
+            email:'',
+            password:'',
+        }
+    }
   render() {
     return (
-      <div>Login</div>
+      <TextInput placeholder="Email" onChangeText={(name)=}/>
     )
   }
 }
